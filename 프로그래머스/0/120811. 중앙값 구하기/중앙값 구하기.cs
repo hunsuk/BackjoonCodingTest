d77d6cc3a1a3c9718b5019ca596f9a8067073b23
side <44570515+hunsuk@users.noreply.github.com>
2024-08-09 14:05:39 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.35 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Array.Sort(array);
+        return array[array.Length / 2];
+    }
+}
