eadfa58d874f0b643a124beb385eb54227dd0ce1
side <44570515+hunsuk@users.noreply.github.com>
2024-08-16 11:08:54 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int x = -999; x <= 999; x++) {
+		for (int y = -999; y <= 999; y++) {
+			
+			if ((a * x + b * y) == c && (d * x + e * y) == f) {
+				cout << x << " " << y << endl;
+			}
+
+		}
+	}
+}
